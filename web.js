568fe3ef070580bc21b2dd3f638b6620831f259a
ccjinyang <jybinary@gmail.com>
2013-07-11 14:16:10 -0700

    updated

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 var output=express.readFileSync('index.html');
 app.get('/', function(request, response) {
-  response.send(output.toString);
+  response.send("Hello World 2!");
 });
 
 var port = process.env.PORT || 5000;
